9e0820a5f1fc73afab578ed05811edf035845ea3
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2020-09-05 09:22:07 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'd14-themes-static-css';
+  setTheme(cssFile: string) {
+    /**
+     * code DOM ko cần add thêm bất kỳ thư viện nào
+     * <HTMLLinkElement>: là ép kiểu trên TypeScript => javaScript ko cần
+     * Nếu ko cast thì ".href" sẽ bão lỗi ko tìm thấy properties
+     * <a> => HTMLLinkElement
+     * <input> => HTMLInputElement
+     */
+    (<HTMLLinkElement>document.getElementById('stylesheet')).href = cssFile;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Child1Component } from './child1/child1.component';
+import { Child2Component } from './child2/child2.component';
+//Material module
+import { MaterialModule } from './material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, Child1Component, Child2Component],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
