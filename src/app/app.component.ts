ce7ae80f826139809fb50ffc719c1b04d62a3aa6
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2021-01-19 15:41:32 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  /**
+   * để tránh trong thời gian download new css theme sẽ bị mất theme
+   * ta dùng ajax download css ở background trc (css có cached).
+   * nếu download thành công thì tiến hành đổi theme (nếu fail thì vẫn dùng theme cũ)
+   */
   setTheme(cssFile: string) {
     /**
      * code DOM ko cần add thêm bất kỳ thư viện nào
